1e73253092f0c003b6acea1fd1b1aa87168bb582
Joshua R Sanders <joshua_sandersr@yahoo.com>
2015-11-23 00:19:55 -0800

    Implement parts of the game logic
    - PopulateLegalCurrentMoves
        This function is called on startup and turn switch.
        Taking into account the current player, it will find
        all possible moves and jumps and place them accordingly.
    
    - Convenience function for printing BoardIndex
    
    - Fixed the move validity code
    
    - CheckersMoveLauncher now has a CheckersMove instead of
      a source and destination separately
    
    - Placed a good amount of logging in useful places

--- sfml-checkers/sfml-checkers/Game.cpp
+++ sfml-checkers/sfml-checkers/Game.cpp
@@ -8,9 +8,21 @@
 
 #include <assert.h>
 
+namespace {
+	// White player will move South
+	const int s_south = 1;
+
+	// Black player moves North.
+	const int s_north= -1;
+
+	const int s_west = -1;
+	const int s_east = 1;
+}
+
 Game::Game()
 	: m_boardData(s_boardSize, std::vector<PieceDisplayType>(s_boardSize, EMPTY))
 	, m_pendingMoveLauncher(new CheckersMoveLauncher(std::bind(&Game::OnMoveSelectionFinalized, this)))
+	, m_whitePlayerTurn(true)
 {
 	Setup();
 }
@@ -24,18 +36,36 @@ BoardIndex Game::GetBoardIndexFromRowCol(int row, int col) const
 	if (!IsValidBoardIndex(std::make_pair(row, col)))
 		return BoardIndex();
 
-	return std::make_pair(row, col);
+	// Row and column are flipped in the UI as opposed to our data.
+	return std::make_pair(col, row);
 }
 
 void Game::OnMoveSelectionEvent(const BoardIndex& boardIndex)
 {
 	assert(IsValidBoardIndex(boardIndex));
 
+	// Move source must contain a piece.
+	if (!m_pendingMoveLauncher->IsSourceSet() && !ContainsPiece(boardIndex))
+	{
+		LOG_DEBUG_CONSOLE("Error: Move source does not contain a piece: "
+			+ BoardIndexToString(boardIndex));
+		return;
+	}
+
 	m_pendingMoveLauncher->HandleMoveSelected(boardIndex);
 }
 
 void Game::OnMoveSelectionFinalized()
 {
+	const BoardIndex& moveSource = m_pendingMoveLauncher->GetMoveSource();
+	const BoardIndex& moveDestination = m_pendingMoveLauncher->GetMoveDestination();
+
+	LOG_DEBUG_CONSOLE("Info: Attempting to move " + BoardIndexToString(moveSource) + " to "
+		+ BoardIndexToString(moveDestination));
+
+	// TODO: IsLegalMove
+		// Move()
+
 	m_pendingMoveLauncher.reset(new CheckersMoveLauncher(std::bind(&Game::OnMoveSelectionFinalized, this)));
 }
 
@@ -57,6 +87,23 @@ void Game::Setup()
 			}
 		}
 	}
+	PopulateLegalCurrentMoves();
+}
+
+void Game::PopulateLegalCurrentMoves()
+{
+	for (int row = 0; row < s_boardSize; ++row)
+	{
+		for (int col = 0; col < s_boardSize; ++col)
+		{
+			BoardIndex currentIndex = std::make_pair(row, col);
+			PieceDisplayType currentPiece = m_boardData[row][col];
+			if (ContainsPiece(currentIndex) && IsPieceOfCurrentPlayer(currentPiece))
+			{
+				EvaluatePossibleMovesForIndex(currentIndex);
+			}
+		}
+	}
 }
 
 bool Game::IsValidBoardIndex(const BoardIndex& boardIndex) const
@@ -64,15 +111,132 @@ bool Game::IsValidBoardIndex(const BoardIndex& boardIndex) const
 	int row = boardIndex.first;
 	int col = boardIndex.second;
 
-	bool isValid = row >= 0 || row < s_boardSize || col >= 0 || col < s_boardSize;
+	bool isValid = row >= 0 && row < s_boardSize && col >= 0 && col < s_boardSize;
 
 	if (!isValid)
 	{
-		LOG_DEBUG_CONSOLE("Error: attempt to access invalid index" + std::to_string(row)
-			+ " , " + std::to_string(col));
+		LOG_DEBUG_CONSOLE("Warning: attempt to access invalid index " + BoardIndexToString(boardIndex));
 	}
 
 	return isValid;
+}
+
+bool Game::IsLegalMove(const CheckersMove& move) const
+{
+	// TODO: Return whether this move appears in the jump list or the move list.
+
+	return true;
+}
+
+bool Game::ContainsPiece(const BoardIndex& boardIndex) const
+{
+	return IsValidBoardIndex(boardIndex)
+		&& m_boardData[boardIndex.first][boardIndex.second] != EMPTY;
+}
+
+bool Game::IsPieceOfCurrentPlayer(PieceDisplayType piece) const
+{
+	return m_whitePlayerTurn && (piece == WHITE || piece == WHITE_KING)
+		|| !m_whitePlayerTurn && (piece == BLACK || piece == BLACK_KING);
+}
+
+void Game::EvaluatePossibleMovesForIndex(const BoardIndex& boardIndex)
+{
+	LOG_DEBUG_CONSOLE("Info: Evaluating moves for " + BoardIndexToString(boardIndex));
+
+	switch (m_boardData[boardIndex.first][boardIndex.second])
+	{
+	case BLACK:
+		AddValidMovesForDirection(boardIndex, s_north, s_west);
+		AddValidMovesForDirection(boardIndex, s_north, s_east);
+		break;
+	case WHITE:
+		AddValidMovesForDirection(boardIndex, s_south, s_west);
+		AddValidMovesForDirection(boardIndex, s_south, s_east);
+		break;
+	case BLACK_KING:
+	case WHITE_KING:
+		AddValidMovesForDirection(boardIndex, s_north, s_west);
+		AddValidMovesForDirection(boardIndex, s_north, s_east);
+		AddValidMovesForDirection(boardIndex, s_south, s_west);
+		AddValidMovesForDirection(boardIndex, s_south, s_east);
+		break;
+	case EMPTY:
+	default:
+		break;
+	}
+}
+
+void Game::AddValidMovesForDirection(const BoardIndex& currentPosition, int verticalDirection,
+	int horizontalDirection)
+{
+	BoardIndex newPosition = std::make_pair(currentPosition.first + verticalDirection,
+		currentPosition.second + horizontalDirection);
+
+	if (!IsValidBoardIndex(newPosition))
+		return;
+
+	// Normal move.
+	if (!ContainsPiece(newPosition))
+	{
+		LOG_DEBUG_CONSOLE("Info: Added Valid Destination: " + BoardIndexToString(newPosition));
+		m_legalDestinations.push_back(GetCheckersMove(currentPosition, newPosition));
+	}
+
+	// Possibly a jump
+	else if (!IsPieceOfCurrentPlayer(GetPieceForIndex(newPosition)))
+	{
+		AddValidJumpForDirection(currentPosition, verticalDirection, horizontalDirection);
+	}
+}
+
+void Game::AddValidJumpForDirection(const BoardIndex& currentPosition, int verticalDirection,
+	int horizontalDirection)
+{
+	// Look ahead an extra space in in the same direction.
+	BoardIndex possibleJump = std::make_pair(currentPosition.first + (verticalDirection * 2),
+		currentPosition.second + (horizontalDirection * 2));
+
+	if (!IsValidBoardIndex(possibleJump))
+		return;
+
+	if (!ContainsPiece(possibleJump))
+	{
+		LOG_DEBUG_CONSOLE("Info: Added Valid Jump Destination: " + BoardIndexToString(possibleJump));
+		m_legalJumpDestinations.push_back(GetCheckersMove(currentPosition, possibleJump));
+	}
+}
+
+std::string Game::BoardIndexToString(const BoardIndex& index) const
+{
+	return std::string(std::to_string(index.first) +  " , ") + std::to_string(index.second);
+}
+
+PieceDisplayType Game::GetPieceForIndex(const BoardIndex& index) const
+{
+	return m_boardData[index.first][index.second];
+}
+
+CheckersMove Game::GetCheckersMove(const BoardIndex& sourceIndex, const BoardIndex& destinationIndex) const
+{
+	return std::make_pair(sourceIndex, destinationIndex);
+}
+
+void Game::MovePiece()
+{
+	// TODO: Move the piece.
+	OnPieceMoved();
+}
+
+void Game::OnPieceMoved()
+{
+	SwitchTurns();
+	PopulateLegalCurrentMoves();
+}
+
+void Game::SwitchTurns()
+{
+	m_whitePlayerTurn = !m_whitePlayerTurn;
 }
 
 void Game::DEBUG_PRINT_BOARD()
@@ -109,8 +273,7 @@ void Game::DEBUG_PRINT_BOARD()
 //---------------------------------------------------------------
 
 CheckersMoveLauncher::CheckersMoveLauncher(std::function<void()> launchMoveCallback)
-	: m_moveSource(-1, -1)
-	, m_moveDestination(-1, -1)
+	: m_checkersMove(BoardIndex(-1,-1), BoardIndex(-1,-1))
 	, m_isSourceSet(false)
 	, m_isDestinationSet(false)
 	, m_launchMoveCallback(launchMoveCallback)
@@ -134,13 +297,13 @@ void CheckersMoveLauncher::HandleMoveSelected(const BoardIndex& move)
 	// The first selected move is the source.
 	if (!m_isSourceSet)
 	{
-		m_moveSource = move;
+		m_checkersMove.first = move;
 		m_isSourceSet = true;
 	}
 	else
 	{
 		// Next is the destination.
-		m_moveDestination = move;
+		m_checkersMove.second = move;
 		m_isDestinationSet = true;
 
 		// Ready to move, launch!
@@ -150,10 +313,15 @@ void CheckersMoveLauncher::HandleMoveSelected(const BoardIndex& move)
 
 const BoardIndex& CheckersMoveLauncher::GetMoveSource() const
 {
-	return m_moveSource;
+	return m_checkersMove.first;
 }
 
 const BoardIndex& CheckersMoveLauncher::GetMoveDestination() const
 {
-	return m_moveDestination;
+	return m_checkersMove.second;
+}
+
+bool CheckersMoveLauncher::IsSourceSet() const
+{
+	return m_isSourceSet;
 }
--- sfml-checkers/sfml-checkers/Game.h
+++ sfml-checkers/sfml-checkers/Game.h
@@ -12,6 +12,9 @@
 
 class CheckersMoveLauncher;
 
+// Source to destination.
+typedef std::pair<BoardIndex, BoardIndex> CheckersMove;
+
 class Game
 {
 public:
@@ -19,8 +22,11 @@ public:
 	~Game();
 
 	const BoardData& GetBoardData() { return m_boardData; }
+
+	// Return the correct board index for UI coordinates.
 	BoardIndex GetBoardIndexFromRowCol(int row, int col) const;
 
+	// Called in response to the UI reporting that a player made a selection.
 	void OnMoveSelectionEvent(const BoardIndex& boardIndex);
 
 	// This is called from the CheckersMoveLauncher when the move destination is selected.
@@ -28,8 +34,52 @@ public:
 	void OnMoveSelectionFinalized();
 
 private:
+	// Setup pieces in initial position and other initialization activities.
 	void Setup();
+
+	// For the current player, generate a list of all legal move possibilities.
+	void PopulateLegalCurrentMoves();
+
+	// Returns whether the index is a valid board index.
 	bool IsValidBoardIndex(const BoardIndex& boardIndex) const;
+
+	// Returns whether the move is legal according to checkers moves.
+	bool IsLegalMove(const CheckersMove& move) const;
+
+	// Returns whether the specified index contains a piece.
+	bool ContainsPiece(const BoardIndex& boardIndex) const;
+
+	// Returns whether the specified piece belongs to the player whose turn it currently is.
+	bool IsPieceOfCurrentPlayer(PieceDisplayType piece) const;
+
+	// This will attempt to add possible moves for a specific piece.
+	void EvaluatePossibleMovesForIndex(const BoardIndex& pieceIndex);
+
+	// Given a direction, will add a move to the appropriate list if it is valid.
+	void AddValidMovesForDirection(const BoardIndex& currentPosition, int verticalDirection,
+		int horizontalDirection);
+
+	// Given a direction, will add a valid jump to the jump list.
+	void AddValidJumpForDirection(const BoardIndex& currentPosition, int verticalDirection,
+		int horizontalDirection);
+
+	// Will return a string representation of a BaordIndex.
+	std::string BoardIndexToString(const BoardIndex& index) const;
+
+	// Returns PieceDisplayType for given index.
+	PieceDisplayType GetPieceForIndex(const BoardIndex& index) const;
+
+	// Given a source and a destination will return a CheckersMove.
+	CheckersMove GetCheckersMove(const BoardIndex& sourceIndex, const BoardIndex& destinationIndex) const;
+
+	// Will actually move the piece and take care of cleaning up dead pieces/promotions.
+	void MovePiece();
+
+	// Called in response to a piece successfully moving.
+	void OnPieceMoved();
+
+	// This will toggle player turns
+	void SwitchTurns();
 
 	void DEBUG_PRINT_BOARD();
 
@@ -40,10 +90,14 @@ private:
 	std::unique_ptr<CheckersMoveLauncher> m_pendingMoveLauncher;
 
 	// This list is populated each turn and represents all possible moves of that player.
-	std::vector<BoardIndex> m_possibleMoveDestinations;
-};
+	std::vector<CheckersMove> m_legalDestinations;
 
-//---------------------------------------------------------------
+	// If this list is not empty, a move must come from this list.
+	std::vector<CheckersMove> m_legalJumpDestinations;
+
+	// Toggle value. If it is not white player's turn, it is black players turn.
+	bool m_whitePlayerTurn;
+};
 
 class CheckersMoveLauncher
 {
@@ -57,9 +111,10 @@ public:
 	const BoardIndex& GetMoveSource() const;
 	const BoardIndex& GetMoveDestination() const;
 
+	bool IsSourceSet() const;
+
 private:
-	BoardIndex m_moveSource;
-	BoardIndex m_moveDestination;
+	CheckersMove m_checkersMove;
 
 	bool m_isSourceSet;
 	bool m_isDestinationSet;
