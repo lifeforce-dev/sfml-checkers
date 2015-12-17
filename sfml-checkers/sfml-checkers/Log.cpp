b2ef9adc09b3ecf055206ddb67c719ac95b267aa
Joshua R Sanders <joshua_sandersr@yahoo.com>
2015-12-17 17:52:19 -0600

    Removed unnecessary logging things

--- sfml-checkers/sfml-checkers/Log.cpp
+++ sfml-checkers/sfml-checkers/Log.cpp
@@ -21,48 +21,6 @@ enum MessageType
 	CONSOLE
 };
 
-void WrapMessage(std::string& message)
-{
-	// No need to wrap.
-	if (message.empty() || message.length() < s_maxLength)
-		return;
-
-	std::stringstream ss;
-	while (message.length() > s_maxLength)
-	{
-		// Split message into chunk separated by new lines.
-		std::string chunk = message.substr(0, s_maxLength);
-		message.erase(0, s_maxLength);
-
-		ss << chunk;
-		ss << '\n';
-	}
-
-	// Push the remaining bits of the message into the buffer.
-	ss << message;
-	message = ss.str();
-}
-
-void AddMessageGuards(std::string& message)
-{
-	if (message.empty())
-		return;
-
-	std::stringstream ss;
-
-	ss << '\n';
-	for (int i = 0; i <= std::min(static_cast<int>(message.length()), s_maxLength); ++i)
-	{
-		ss << '-';
-	}
-	ss << '\n';
-
-	std::string guard = ss.str();
-
-	ss << message << guard;
-	message = ss.str();
-}
-
 void AppendLoggingInfoToLog(const std::string& fileName, int lineNumber, std::string& messageOut)
 {
 	// Remove full path from the filename.
